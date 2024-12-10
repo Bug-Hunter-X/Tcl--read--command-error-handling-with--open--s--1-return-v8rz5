6c249a8f78330b38e01a955db0a27d7dcf363f48
BugHunter-X <askzenai@gmail.com>
2024-12-10 06:44:06 +0100

    Add bug.tcl file

---
+++ bug.tcl
@@ -0,0 +1,10 @@
+proc read_file {filename} { 
+  set fp [open $filename r]
+  if {$fp == -1} { return -1 }
+  set content [read $fp]
+  close $fp
+  return $content
+}
+
+set content [read_file non_existent_file.txt]
+if {$content == -1} { puts "File not found" } else { puts $content }
